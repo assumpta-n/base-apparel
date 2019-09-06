7b574d1876ed7bd6d98941b485328518924b92fc
Assumpta Nalubowa <assumptanalubowa@gmail.com>
2019-09-06 09:50:10 +0300

    Prevents form from reloading page(Default bhaviour) when submit button is clicked

--- js/app.js
+++ js/app.js
@@ -1,6 +1,11 @@
 function submitEmail() {
+    preventDefaultEvent()
     let feedBackEls = document.getElementsByClassName('toggle-visibility')
     Array.prototype.forEach.call(feedBackEls, feedBackEl => {
         feedBackEl.style.visibility = 'hidden'
     })
+}
+function preventDefaultEvent() {
+    let event = window.event
+    event.preventDefault()
 }
